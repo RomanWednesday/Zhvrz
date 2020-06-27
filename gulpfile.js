8989570ca1dc1c4b2ec0cce264b77ded2d72eba6
Roman Dmitriev <romanw652@gmail.com>
2020-06-27 22:06:53 +0300

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+
+gulp.task('sass', function (done) {
+    gulp.src('scss/**/*.scss')
+        .pipe(sass({ outputStyle: 'compact' }).on('error', sass.logError)) // Using gulp-sass
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer(['last 10 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(concat('bundle.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+
+    done();
+});
+
+gulp.task('serve', function (done) {
+
+    browserSync.init({
+        server: 'dist',
+        notify: false
+    });
+
+    gulp.watch("scss/*.scss", gulp.series('sass'));
+    gulp.watch("dist/*.html").on('change', () => {
+        browserSync.reload()
+        done();
+    });
+    done();
+})
+
+
+
+
+gulp.task('default', gulp.series(['sass', 'serve']));
+
