8fe5056776f813f863d0fed66fc8a81add5eec04
Roman Dmitriev <romanw652@gmail.com>
2020-07-06 22:51:20 +0300

    Закончено бургер меню

--- dist/js/handlers.js
+++ dist/js/handlers.js
@@ -6,3 +6,11 @@ menuBtn.addEventListener('click', () => {
     humburgerBtn.classList.toggle('open')
     menu.classList.toggle('opened');
 })
+
+menu.addEventListener('click', (e) => {
+    if (e.target.classList[0] === 'menu-item' && e.target.classList[1] === 'expanded') {
+        e.target.parentNode.classList.toggle('opened');
+        e.target.classList.toggle('opened');
+    }
+})
+
