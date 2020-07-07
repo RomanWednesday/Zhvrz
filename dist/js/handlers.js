7c0545f925db323fd14e176576a48b163f96e4f0
Roman Dmitriev <romanw652@gmail.com>
2020-07-07 03:59:39 +0300

    Добавил меню поиска, меню запроса. Поправил ui-kit

--- dist/js/handlers.js
+++ dist/js/handlers.js
@@ -1,8 +1,36 @@
 const menuBtn = document.querySelector('.aside-open-menu');
-const menu = document.querySelector('.burger-menu');
+const searchBtn = document.querySelector('.aside-search');
+const mailBtn = document.querySelector('.aside-mail');
 const humburgerBtn = document.querySelector('.burger-box')
+const menu = document.querySelector('#alt-menu-1');
+const searchMenu = document.querySelector('#alt-menu-2');
+const contactMenu = document.querySelector('#alt-menu-3');
+
+function closeOtherModal() {
+    if (searchMenu.classList[1]) {
+        searchMenu.classList.toggle('opened');
+        searchBtn.classList.toggle('opened');
+    }
+    if (menu.classList[1]) {
+        menu.classList.toggle('opened');
+        humburgerBtn.classList.toggle('open')
+    }
+    if (contactMenu.classList[1]) {
+        contactMenu.classList.toggle('opened');
+        mailBtn.classList.toggle('open')
+    }
+}
+
 
 menuBtn.addEventListener('click', () => {
+    if (contactMenu.classList[1]) {
+        contactMenu.classList.toggle('opened');
+        mailBtn.classList.toggle('open')
+    }
+    if (searchMenu.classList[1]) {
+        searchMenu.classList.toggle('opened');
+        searchBtn.classList.toggle('opened');
+    }
     humburgerBtn.classList.toggle('open')
     menu.classList.toggle('opened');
 })
@@ -12,5 +40,31 @@ menu.addEventListener('click', (e) => {
         e.target.parentNode.classList.toggle('opened');
         e.target.classList.toggle('opened');
     }
+})
+
+searchBtn.addEventListener('click', () => {
+    if (menu.classList[1]) {
+        menu.classList.toggle('opened');
+        humburgerBtn.classList.toggle('open')
+    }
+    if (contactMenu.classList[1]) {
+        contactMenu.classList.toggle('opened');
+        mailBtn.classList.toggle('open')
+    }
+    searchBtn.classList.toggle('opened');
+    searchMenu.classList.toggle('opened');
+})
+
+mailBtn.addEventListener('click', () => {
+    if (searchMenu.classList[1]) {
+        searchMenu.classList.toggle('opened');
+        searchBtn.classList.toggle('opened');
+    }
+    if (menu.classList[1]) {
+        menu.classList.toggle('opened');
+        humburgerBtn.classList.toggle('open')
+    }
+    contactMenu.classList.toggle('opened');
+    mailBtn.classList.toggle('open')
 })
 
